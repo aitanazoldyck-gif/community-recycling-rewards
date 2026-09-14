229d81cb3c8c8faec5fc0fa2cf57e64425206b3e
aitanazoldyck-gif <aitanazoldyck@gmail.com>
2026-09-14 15:18:28 +0800

    commit

--- src/components/resident/redeem-card.tsx
+++ src/components/resident/redeem-card.tsx
@@ -88,7 +88,7 @@ export function RedeemCard({ balance }: { balance: number }) {
         <div className="flex flex-col gap-4 sm:flex-row sm:items-start sm:justify-between">
           <div className="flex items-center gap-3">
             <Image
-              src="/GCash_Logo.svg"
+              src="/gcash.svg"
               alt="GCash"
               width={48}
               height={48}
